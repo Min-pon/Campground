edd9d8c80244ef9c1e58a72bd2a2bf9104078f31
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-04-14 08:48:06 +0700

    Add campground controller

---
+++ controllers/campgrounds.js
@@ -0,0 +1,81 @@
+const Campground = require("../models/Campground");
+
+//@desc     Get all campgrounds
+//@route    GET /api/v1/campgrounds
+//@access   Public
+exports.getCampgrounds = async (req, res, next) => {
+  try {
+    const campgrounds = await Campground.find();
+    res
+      .status(200)
+      .json({ success: true, count: campgrounds.length, data: campgrounds });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
+
+//@desc     Get single campground
+//@route    GET /api/v1/camgrounds/:id
+//@access   Public
+exports.getCampground = async (req, res, next) => {
+  try {
+    const campground = await Campground.findById(req.params.id);
+
+    if (!campground) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: campground });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
+
+//@desc     Create new campground
+//@route    POST /api/v1/campgrounds
+//@access   Private
+exports.createCampground = async (req, res, next) => {
+  const campground = await Campground.create(req.body);
+  res.status(201).json({ success: true, data: campground });
+};
+
+//@desc     Update single campground
+//@route    PUT /api/v1/campgrounds/:id
+//@access   Private
+exports.updateCampground = async (req, res, next) => {
+  try {
+    const campground = await Campground.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!campground) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: hospital });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
+
+//@desc     Delete single campground
+//@route    DELETE /api/v1/campgrounds/:id
+//@access   Public
+exports.deleteCampground = async (req, res, next) => {
+  try {
+    const campground = await Campground.findByIdAndDelete(req.params.id);
+
+    if (!campground) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,32 +1,18 @@
 const express = require("express");
+const {
+  getCampgrounds,
+  getCampground,
+  createCampground,
+  updateCampground,
+  deleteCampground,
+} = require("../controllers/campgrounds");
 const router = express.Router();
 
-const app = express();
-
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all campgrounds" });
-});
-
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show campground ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new campground" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update campground ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete campground ${req.params.id}` });
-});
+router.route("/").get(getCampgrounds).post(createCampground);
+router
+  .route("/:id")
+  .get(getCampground)
+  .put(updateCampground)
+  .delete(deleteCampground);
 
 module.exports = router;

5cbb3a856913c27b3010ff40174f5d9d20af9c20
Nichapa Nittayasittikun <Nichapanit555@gmail.com>
2024-04-20 13:05:19 +0700

    add login and logout function

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ const sendTokenResponse = (user, statusCode, res) => {
     })
 };
 
-//@desc Register User
+//@desc Register user
 //@route POST ??/auth/register
 //@access Public
 exports.register = async (req, res, next) => {
@@ -40,4 +40,59 @@ exports.register = async (req, res, next) => {
     }
 };
 
-//@desc 
+//@desc Login user
+//@route POST ??/auth/login
+//@access Public
+exports.login = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+
+        //Validate email & password
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                msg: 'Please provide an email and password'
+            });
+        }
+
+        //Check for user
+        const user = await User.findOne({email}).select('+password');
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                msg: 'Invalid credentials'
+            });
+        }
+
+        //Check if password matches
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch){
+            return res.status(401).json({
+                success: false,
+                msg: 'Invalid credentials'
+            });
+        }
+
+        sendTokenResponse(user, 200, res);
+    }catch (err){
+        return res.status(401).json({
+            success: false,
+            msg: "Cannot convert email or password to string"
+        });
+    }
+    
+};
+
+//@desc Log user out / clear cookie
+//@route GET ??/auth/logout
+//@access Private
+exports.logout = async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10*1000),
+        httpOnly: true
+    });
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+};

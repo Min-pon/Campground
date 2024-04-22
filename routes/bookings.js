906c0aaa79c8a531d59897f63830ef391acd941e
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-04-22 16:52:52 +0700

    update booking router

--- routes/bookings.js
+++ routes/bookings.js
@@ -3,15 +3,21 @@ const {
   getBookings,
   addBooking,
   getBooking,
+  updateBooking,
+  deleteBooking,
 } = require("../controllers/bookings");
 
 const { protect, authorize } = require("../middleware/auth");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route("/")
   .get(protect, getBookings)
   .post(protect, authorize("admin", "user"), addBooking);
-router.route("/:id").get(getBooking);
+router
+  .route("/:id")
+  .get(protect, getBooking)
+  .put(protect, authorize("admin", "user"), updateBooking)
+  .delete(protect, authorize("admin", "user"), deleteBooking);
 module.exports = router;

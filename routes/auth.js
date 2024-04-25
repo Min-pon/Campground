30d36cbf233c15217a479649b6dd4ec5c4d55ff2
Nichapa Nittayasittikun <Nichapanit555@gmail.com>
2024-04-25 11:40:20 +0700

    add delete account function

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const {register, login, getInfo, editInfo, logout} = require('../controllers/auth');
+const {register, login, getInfo, editInfo, logout, deleteAccount} = require('../controllers/auth');
 
 const router = express.Router();
 
-const {protect} = require('../middleware/auth');
+const {protect, authorize} = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login',login);
-router.get('/info', protect, getInfo).put('/info', protect, editInfo);
+router.get('/info', protect, getInfo).put('/info', protect, authorize('user', 'admin'), editInfo);
 router.get('/logout', logout);
+router.delete('/delete', protect, authorize('user', 'admin'), deleteAccount)
 
 module.exports = router;
